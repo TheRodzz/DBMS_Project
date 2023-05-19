aaa0dbb39d47741c7f028997674d41ae1db8349f
Vidhu Arora <vidhu.arora@iiitb.ac.in>
2023-05-19 22:40:53 +0530

    Created tables and added sample data

--- src/ott.sql
+++ src/ott.sql
@@ -3,4 +3,77 @@ CREATE DATABASE ottplatform;
 USE ottplatform;
 
 -- creating tables
+CREATE TABLE User (
+    uid INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    isAdmin BOOL NOT NULL
+);
 
+CREATE TABLE Media (
+    tid INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    release_date DATE NOT NULL
+);
+
+CREATE TABLE Languages(
+    tid INT,
+    language VARCHAR(50) NOT NULL,
+    PRIMARY KEY (tid,language),
+    FOREIGN KEY (tid) REFERENCES Media (tid)
+);
+
+CREATE TABLE Ratings (
+    tid INT NOT NULL,
+    uid INT NOT NULL,
+    rating DECIMAL(2,1) NOT NULL,
+    PRIMARY KEY(tid,uid),
+    FOREIGN KEY (tid) REFERENCES Media(tid),
+    FOREIGN KEY (uid) REFERENCES User(uid)
+);
+
+CREATE TABLE View_History (
+    tid INT NOT NULL,
+    uid INT NOT NULL,
+    watched_on TIMESTAMP,
+    PRIMARY KEY(tid,uid,watched_on),
+    FOREIGN KEY (tid) REFERENCES Media(tid),
+    FOREIGN KEY (uid) REFERENCES User(uid)
+);
+
+-- Populating User table
+INSERT INTO User (name, user_name, password, isAdmin) VALUES
+  ('John Doe', 'johndoe', 'password123', 0),
+  ('Jane Smith', 'janesmith', 'pass456', 1),
+  ('Michael Johnson', 'mjohnson', 'securepass', 0);
+
+-- Populating Media table
+INSERT INTO Media (title, release_date) VALUES
+  ('Movie A', '2022-01-10'),
+  ('Movie B', '2021-07-22'),
+  ('TV Show X', '2020-05-05');
+
+-- Populating Languages table
+INSERT INTO Languages (tid, language) VALUES
+  (1, 'English'),
+  (1, 'Spanish'),
+  (2, 'English'),
+  (3, 'English'),
+  (3, 'French');
+
+-- Populating Ratings table
+INSERT INTO Ratings (tid, uid, rating) VALUES
+  (1, 1, 4.5),
+  (1, 2, 3.8),
+  (2, 1, 4.2),
+  (2, 3, 4.0),
+  (3, 2, 3.5),
+  (3, 3, 4.3);
+
+-- Populating View_History table
+INSERT INTO View_History (tid, uid, watched_on) VALUES
+  (1, 1, '2022-02-15 10:30:00'),
+  (1, 2, '2022-02-18 14:45:00'),
+  (2, 1, '2021-08-01 20:15:00'),
+  (3, 3, '2020-07-10 12:00:00');
